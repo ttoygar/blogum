770507859dc6dd0c5e7df1c0beee6f4f14b39daf
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-10-24 15:01:28 +0300

    bootstrap added, rudimentary navbar created, links arranged

--- blogum/views.py
+++ blogum/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render, HttpResponse
 def anasayfa(request):
     # return HttpResponse('Hello World!')
     return render(request, 'blogum/anasayfa.html')
+
+
+def halk_ekmek(request):
+    return  render(request, 'blogum/halk_ekmek.html')
+
+
+def projeler(request):
+    return render(request, 'blogum/projeler.html')
